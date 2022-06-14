a788ea15ff7d07f7380a15ed1e903579f00ce318
Evgeniy Eremin <103576500+revoidWhale@users.noreply.github.com>
2022-06-15 00:56:59 +0700

    Delete src directory

--- src/Link.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-// import { Link } from 'react-router-dom'
-
-const Link = ({ className, children }) => {
-  console.log(children)
-  return <a className={className}>{children}</a>
-}
-
-export default Link
